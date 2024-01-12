b8cd7756753adae5ae1d18021edb51d8b4599dc5
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-12 19:49:19 +0200

    add latest products to home page

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomePageController extends Controller
 {
-    //
+        public function index()
+        {
+            $recent_products = Product::latest()->take(3)->get(['name','price', 'image','description']);
+            return view('index' , compact('recent_products'));
+        }
 }
