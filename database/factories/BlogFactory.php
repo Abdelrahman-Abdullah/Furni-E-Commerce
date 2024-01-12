5a55e65631a1e4f0f6ea80e9e2c0e7006bfdf0da
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-12 20:03:10 +0200

    add blod schema attributes , blog factory logic and seed db

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -16,8 +16,13 @@ class BlogFactory extends Factory
      */
     public function definition(): array
     {
+        $users_id = \App\Models\User::pluck('id')->toArray();
         return [
-            //
+            'author_id' => $this->faker->randomElement($users_id),
+            'title' => $this->faker->sentence(5),
+            'slug' => $this->faker->slug(),
+            'image' => $this->faker->imageUrl(640, 480, 'blogs', true),
+            'description' => $this->faker->paragraph(5),
         ];
     }
 }
--- database/migrations/2024_01_12_174946_create_blogs_table.php
+++ database/migrations/2024_01_12_174946_create_blogs_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('author_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('title', 100)->unique();
+            $table->string('slug', 100)->unique();
+            $table->string('image', 100)->nullable();
+            $table->text('description');
             $table->timestamps();
         });
     }
