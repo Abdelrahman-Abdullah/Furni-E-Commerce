d902bf94883dabd6f0c100303bafb66cd26c9e3d
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-14 20:31:06 +0200

    add the relation between the peoducts and category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -25,5 +26,10 @@ class Product extends Model
         return Attribute::make(
             get: fn() => asset('front-assets/images'.$this->image),
         );
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 }
