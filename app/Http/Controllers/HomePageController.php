ea0495a41a38c4c2355cf626a7fa4a0a021633bc
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-16 22:18:43 +0200

    Just Try New Things Then Deleted

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
-use Illuminate\Foundation\Application;
 use App\Models\{
     Blog,
     Product
@@ -12,13 +9,10 @@ use App\Models\{
 
 class HomePageController extends Controller
 {
-    public function __invoke(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    public function __invoke()
     {
         $recentProducts = Product::latest()->take(3)->get(['name', 'price', 'image', 'description']);
-        $recentBlogs = Blog::with('author:id,name')
-            ->latest()
-            ->take(3)
-            ->get(['title', 'image', 'description', 'author_id','created_at']);
+        $recentBlogs = Blog::with('author:id,name')->latest()->take(3)->get(['title', 'image', 'description', 'author_id', 'created_at']);
 
         return view('index', compact('recentProducts', 'recentBlogs'));
     }
--- app/Services/ProductsService.php
+++ app/Services/ProductsService.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 use App\Models\Product;
-use Illuminate\Database\Eloquent\Collection;
 
 class ProductsService
 {
