8e936b946abea8a9f09768aaf0818f07dc61bf04
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-19 14:28:39 +0200

    update the products controller make the method not invoke and add the apiroute

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -5,18 +5,18 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Services\ProductsService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function __construct(protected ProductsService $productsService){}
 
-    public function __invoke()
+    public function index(): JsonResponse
     {
         $allProducts = $this->productsService->all()->get();
         return response()->json(
             ProductResource::collection($allProducts)
         );
-
     }
 }
