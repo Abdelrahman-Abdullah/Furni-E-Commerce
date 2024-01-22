f9db45368fba7440ed8fb3bacc753df0ca7b5772
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-22 19:29:44 +0200

    add user register view

---
+++ resources/views/Front/users/register.blade.php
@@ -0,0 +1,61 @@
+@extends('Front.layouts.front-layout', ['title' => 'Join Us'])
+@section('content')
+    <div class="untree_co-section">
+        <div class="container">
+
+            <div class="block">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-8 pb-4">
+                        <div class="row mb-5">
+                            <h1>
+                                Register
+                            </h1>
+                        </div>
+
+                        <form>
+                            <div class="row mb-2">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label class="text-black" for="fname">First name</label>
+                                        <input type="text" class="form-control" id="fname">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label class="text-black" for="lname">Last name</label>
+                                        <input type="text" class="form-control" id="lname">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group mb-2">
+                                <label class="text-black" for="email">Email address</label>
+                                <input type="email" class="form-control" id="email">
+                            </div>
+
+                            <div class="row mb-2">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label class="text-black" for="fname">Password</label>
+                                        <input type="text" class="form-control" id="fname">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <label class="text-black" for="lname">Confirm Password</label>
+                                        <input type="text" class="form-control" id="lname">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <a href="www.facbook.com">Have an account ?</a>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary-hover-outline">Sign up</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+@endsection
