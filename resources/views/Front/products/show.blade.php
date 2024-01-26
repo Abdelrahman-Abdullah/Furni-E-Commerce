6180b388d11617f38bb8663ef74159f29cffd20a
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-26 22:09:36 +0200

    add single product view

---
+++ resources/views/Front/products/show.blade.php
@@ -0,0 +1,28 @@
+@extends('Front.layouts.front_layout' , ['title'=>$product->name ?? 'Product Page'])
+@section('content')
+    <div class="untree_co-section product-section before-footer-section">
+        <div class="container">
+            <div class="row">
+                <!-- Start Column 1 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <img src="images/product-3.png" class="img-fluid product-thumbnail">
+                </div>
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>	<h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>	<h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>	<h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>	<h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>	<h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>	<h3 class="product-title">Nordic Chair</h3>
+                    <strong class="product-price">$50.00</strong>
+                </div>
+                <!-- End Column 1 -->
+            </div>
+        </div>
+    </div>
+@endsection
