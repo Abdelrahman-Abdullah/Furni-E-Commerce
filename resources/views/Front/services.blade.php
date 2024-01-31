8904cf959fb81ec8d3b89caa8b8b2c1600e1157b
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-31 18:40:58 +0200

    add service view page

---
+++ resources/views/Front/services.blade.php
@@ -0,0 +1,6 @@
+@extends('Front.layouts.front-layout' , ['title' => 'Services'])
+@section('content')
+
+    @include('Front.partials.why-us')
+    @include('Front.partials.testimonial')
+@endsection
