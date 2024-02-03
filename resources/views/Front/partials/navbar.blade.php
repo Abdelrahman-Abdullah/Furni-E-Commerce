e4dbf445029e64ec906a7d0a1a348ddf673505f7
abdelra7man <abdelra7manabdullah@gmail.com>
2024-02-03 20:18:22 +0200

    update the navbar links add route for home page and blog page

--- resources/views/Front/partials/navbar.blade.php
+++ resources/views/Front/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
     <div class="container">
-        <a class="navbar-brand" href="index.html">Furni<span>.</span></a>
+        <a class="navbar-brand" href="{{route('home')}}">Furni<span>.</span></a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni"
                 aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,7 +21,9 @@
                 <li class="{{request()->is('services') ? 'active' : '' }}">
                     <a class="nav-link" href="{{route('services')}}">Services</a>
                 </li>
-                <li><a class="nav-link" href="blog.html">Blog</a></li>
+                <li class="{{request()->is('blogs') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{route('blogs.index')}}">Blog</a>
+                </li>
                 <li class="{{request()->is('contact') ? 'active' : '' }}">
                     <a class="nav-link" href={{route('contact.create')}}>Contact us</a>
                 </li>
