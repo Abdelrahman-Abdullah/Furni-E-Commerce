c399e1e5c8a36f5dc4aad853971261510e630a7d
abdelra7man <abdelra7manabdullah@gmail.com>
2024-02-05 22:14:48 +0200

    add data for the single blog view and make it dynamic

--- resources/views/Front/blogs/show.blade.php
+++ resources/views/Front/blogs/show.blade.php
@@ -1,4 +1,4 @@
-@extends('Front.layouts.front-layout', ['title' => $blog_name ??'Blog'])
+@extends('Front.layouts.front-layout', ['title' => $blog->title ??'Blog'])
 @section('content')
 
     <div class="blog-section">
@@ -6,21 +6,14 @@
             <div class="row">
                 <div class="col-8 mb-5 w-100">
                     <div class=" d-flex justify-content-evenly">
-                        <a href="#" class="post-thumbnail mx-1   w-50"><img src="images/post-1.jpg" alt="Image" class="w-100"></a>
+                        <span class="post-thumbnail mx-1   w-50"><img src="{{$blog->imageUrl}}" alt="Image" class="w-100"></span>
                         <div class="post-content-entry d-flex flex-column justify-content-center w-50 text-center ">
-                            <h2>First Time Home Owner Ideas</h2>
+                            <h2>{{$blog->title}}</h2>
                             <p class="text-start p-2">
-                                fjdsfbdsnkfhdsfhkkdshfdsjkadk fhdsjfhvdsh fdsjfhsdhflsdlv
-                                fjdsfbdsnkfhdsfhkkdshfdsjkadk fhdsjfhvdsh fdsjfhsdhflsdlv
-                                fjdsfbdsnkfhdsfhkkdshfdsjkadk fhdsjfhvdsh fdsjfhsdhflsdlv
-                                fjdsfbdsnkfhdsfhkkdshfdsjkadk fhdsjfhvdsh fdsjfhsdhflsdlv
-                                fjdsfbdsnkfhdsfhkkdshfdsjkadk fhdsjfhvdsh fdsjfhsdhflsdlv
-                                fjdsfbdsnkfhdsfhkkdshfdsjkadk fhdsjfhvdsh fdsjfhsdhflsdlv
-                                fjdsfbdsnkfhdsfhkkdshfdsjkadk fhdsjfhvdsh fdsjfhsdhflsdlv
-                                fjdsfbdsnkfhdsfhkkdshfdsjkadk fhdsjfhvdsh fdsjfhsdhflsdlv
+                                {{$blog->description}}
                             </p>
                             <div class="meta">
-                                <span>by <a href="#">Kristin Watson</a></span> <span>on Dec 19, 2021</span>
+                                <span>by <a href="#">{{$blog->author->name}}</a></span> <span>{{$blog->createdDiffForHumans}}</span>
                             </div>
                         </div>
                     </div>
