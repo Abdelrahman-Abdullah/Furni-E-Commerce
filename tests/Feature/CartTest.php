d036c72bd4d18b3b459029476506ef0397671b70
abdelra7man <abdelra7manabdullah@gmail.com>
2024-02-14 22:31:53 +0200

    add test for check if the user can increment the product qty in cart

--- tests/Feature/CartTest.php
+++ tests/Feature/CartTest.php
@@ -48,4 +48,27 @@ class CartTest extends TestCase
         // Assert
         $response->assertRedirect('/users/login');
     }
+
+    public function test_user_can_increment_product_quantity_in_cart(): void
+    {
+        // Arrange
+        $category = Category::factory()->create();
+        $product = Product::factory()->create(['category_id' => $category->id]);
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $this->post('/cart/add/'. $product->id);
+        // Act
+        $response = $this->post('/cart/update/'. $product->id , ['increment' => true]);
+        // Assert
+        $response->assertStatus(200);
+        $response->assertSessionHas('cart', [
+            $product->id => [
+                'id' => $product->id,
+                'title' => $product->name,
+                'price' => $product->price,
+                'imageUrl' => $product->image_url,
+                'quantity' => 2
+            ]
+        ]);
+    }
 }
