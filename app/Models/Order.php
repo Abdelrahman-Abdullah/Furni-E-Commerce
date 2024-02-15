fed587cac3c7a50d36bb0eec202b94755d53c51f
abdelra7man <abdelra7manabdullah@gmail.com>
2024-02-15 21:58:20 +0200

    add payment enum status class and cast it for order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Enums\PaymentStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
     use HasFactory;
+    protected $casts = [
+        'transaction_status' => PaymentStatusEnum::class,
+    ];
 }
