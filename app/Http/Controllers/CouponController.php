26b769454bd6c27504b81bc20ea4c9b8da0279c5
abdelra7man <abdelra7manabdullah@gmail.com>
2024-02-22 21:50:03 +0200

    add checkCoupon Method in Coupon Controller

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CouponRequest;
+use App\Models\Coupon;
 use Illuminate\Http\Request;
 
 class CouponController extends Controller
 {
-    //
+    public function checkCoupon(CouponRequest $request)
+    {
+        try {
+            $coupon = Coupon::firstWhere('code', $request->coupon);
+            if (!$coupon || $coupon->validity === 'inactive'){
+                return back()->with('error', 'Invalid Coupon or Expired');
+            }
+            $cart = session('cart' , []);
+            $cart['totalPrice'] *=  ($coupon->value / 100);
+            session(['cart' => $cart]);
+            return back()->with('success', 'Coupon Applied Successfully');
+
+        }catch (\Exception $e){
+            //
+        }
+
+    }
 }
