ae80a6a51d83214ce989d49717925795d2b10f82
abdelra7man <abdelra7manabdullah@gmail.com>
2024-02-22 21:31:09 +0200

    add coupon fillable attributes and enum for status of code

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Enums\CouponStatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
 {
+
     use HasFactory;
+    protected $fillable = ['code', 'value', 'validity'];
+    protected $casts = [
+        'validity' => CouponStatusEnum::class
+    ];
 }
