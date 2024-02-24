42ca040ac4842fee701f485cb33fa8c7a328d0e0
abdelra7man <abdelra7manabdullah@gmail.com>
2024-02-24 13:13:32 +0200

    add mail notification class

--- app/Http/Controllers/ResetPassword.php
+++ app/Http/Controllers/ResetPassword.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ResetPasswordRequest;
+use App\Models\User;
+use App\Notifications\SendEmailOTPCodeCoNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Str;
 
 class ResetPassword extends Controller
 {
@@ -13,7 +17,15 @@ class ResetPassword extends Controller
     }
     public function getOtp(ResetPasswordRequest $request)
     {
-        dd("welome Gere");
+        $user =  User::select('name','id')->where('email', $request->validated())->firstOrFail();
+        // generate Code
+        $code = Str::random(6);
+        // save in session
+        $request->session()->put('otp', $code);
+        Notification::send($user, new SendEmailOTPCodeCoNotification($user));
+        // Send Email To User
+        // Check if the code is right
+        // return view
     }
 
 }
